27445332ad5e6d03a0f1d183c5d2c165f6d1e8c9
Vladislav_Dektiarev <Vladislav_Dektiarev@epam.com>
2017-04-10 18:01:46 +0300

    Add enrollee spring boot application

---
+++ enrollee-service/src/main/java/by/bsu/diploma/enrollee/EnrolleeApplication.java
@@ -0,0 +1,12 @@
+package by.bsu.diploma.enrollee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrolleeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnrolleeApplication.class, args);
+    }
+}
